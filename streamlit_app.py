46f05aa58fa5a4d4e64aa530df448605504e0b45
Mamai-hash <158568543+Mamai-hash@users.noreply.github.com>
2025-10-26 16:19:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ with st.expander ('Data visualization'):
         title='Distribution of Patient Age',
         # This is a good, distinct color for the heart app
         color_discrete_sequence=['#ff4b4b'] ) 
-    fig.update_layout(
+    fig.update_layout
         xaxis_title="Age (Years)",
         yaxis_title="Count of Patients"
     st.plotly_chart(fig, use_container_width=True)
