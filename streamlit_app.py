3d8248a4ff964537d57f06934701fc1527491ea0
Mamai-hash <158568543+Mamai-hash@users.noreply.github.com>
2025-10-26 14:58:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Heart Risk Predictor App')
 
-st.write('This is an app that predicts risk of heart disease')
+st.write('This app buils a machine learning model')
+df = pd.read_csv('https://raw.githubusercontent.com/Mamai-hash/MP_Machinelearning/refs/heads/master/heart_risk_data_CLEANED.xls')
+df
