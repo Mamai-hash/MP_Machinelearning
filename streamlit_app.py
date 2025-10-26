7bb993763b1dc357d1e6f791dd1baa107d1ced96
Mamai-hash <158568543+Mamai-hash@users.noreply.github.com>
2025-10-26 15:14:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,11 @@ with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/Mamai-hash/MP_Machinelearning/refs/heads/master/heart_risk_data_CLEANED.xls')
   df
+
+st.write('**X**')
+X = df.drop('TenYearCHD',axis = 1 )
+X
+
+st.write('**Y**')
+Y = df.TenYearCHD
+Y
