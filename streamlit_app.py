cb6dede23c6bc5bbea91abe71584cff1d0d82944
Mamai-hash <158568543+Mamai-hash@users.noreply.github.com>
2025-10-29 06:46:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,12 +70,12 @@ with st.expander('Data visualization'):
 # Data preparations
 with st.sidebar:
     st.header('input features') 
-    sex = st.selectbox ('sex',('male', 'female'))
-    prevalentstroke = st.selectbox('prevalentstroke', ('No (0)', 'Yes (1)'))
-    age = st.slider('age (years)', min_value=32.0, max_value=70.0, value=49.6, step = 0.1)
-    heartRate = st.slider('Heart Rate (beats)', min_value=40, max_value=120, value=75)
-    sysBP = st.slider('sysBP (mmHg)', min_value=83.5, max_value=295.0, value=132.4)
-    totChol = st.slider('totChol (mg/dL)', min_value=10.07, max_value=696.0, value=236.7)
+    input_sex = st.selectbox ('sex',('male', 'female'))
+    input_prevalentstroke = st.selectbox('prevalentstroke', ('No (0)', 'Yes (1)'))
+    input_age = st.slider('age (years)', min_value=32.0, max_value=70.0, value=49.6, step = 0.1)
+    input_heartRate = st.slider('Heart Rate (beats)', min_value=40, max_value=120, value=75)
+    input_sysBP = st.slider('sysBP (mmHg)', min_value=83.5, max_value=295.0, value=132.4)
+    input_totChol = st.slider('totChol (mg/dL)', min_value=10.07, max_value=696.0, value=236.7)
 
 # 1. ENCODE INPUTS (These must happen inside the function)
     def get_user_data():   
