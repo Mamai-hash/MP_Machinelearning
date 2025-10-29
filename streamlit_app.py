89ddc6e23d42b21118ddc3ff6fa4828c10a97ebc
Mamai-hash <158568543+Mamai-hash@users.noreply.github.com>
2025-10-29 07:28:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ with st.sidebar:
  # Create DataFrame, ensuring the columns match the exact order of the training data (X.columns)
         input_df = pd.DataFrame([data], columns=X.columns, index=[0])
             
-        input_df = get_user_data()
+    input_df = get_user_data()
     
     
     
